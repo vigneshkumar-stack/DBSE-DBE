6fa70293a84fae00a0b348077c2b2e3954d4bb62
Vignesh kumar <vigneshkumardandige@gmail.com>
2026-08-28 11:59:39 +0530

    Add files via upload

---
+++ Skill/(S)-Week3.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE BankDB;
+
+USE BankDB;
+CREATE TABLE Customer (
+    Customer_ID INT PRIMARY KEY,
+    Customer_Name VARCHAR(100) NOT NULL,
+    Phone VARCHAR(15),
+    Email VARCHAR(100),
+    City VARCHAR(50)
+);
+
+CREATE TABLE Account (
+    Account_No INT PRIMARY KEY,
+    Customer_ID INT,
+    Account_Type VARCHAR(20),
+    Balance DECIMAL(12,2) DEFAULT 0,
+    Branch VARCHAR(50),
+
+    FOREIGN KEY (Customer_ID)
+    REFERENCES Customer(Customer_ID)
+);
+
+CREATE TABLE Bank_Transaction (
+    Transaction_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Account_No INT,
+    Transaction_Type VARCHAR(20),
+    Amount DECIMAL(12,2),
+    Transaction_Date DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (Account_No)
+    REFERENCES Account(Account_No)
+);
+
+CREATE TABLE Loan (
+    Loan_ID INT PRIMARY KEY,
+    Customer_ID INT,
+    Loan_Type VARCHAR(30),
+    Loan_Amount DECIMAL(12,2),
+    Interest_Rate DECIMAL(5,2),
+
+    FOREIGN KEY (Customer_ID)
+    REFERENCES Customer(Customer_ID)
+);
+
